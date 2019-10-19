b1b6961de05baa4dce2e2c077a35790f7293df22
Yining Wang <yining.wang@utexas.edu>
2019-10-19 18:20:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # My Code here....
+def map_to_negativize(arr)
+  new_arr = []
+  for i in 0...arr.length do
+    new_arr.push(-1 * arr[i])
+  end
+  new_arr
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  new_arr = []
+  for i in 0...arr.length do
+    new_arr.push(arr[i] * 2)
+  end
+  new_arr
+end
+
+def map_to_square(arr)
+  new_arr = []
+  for i in 0...arr.length do
+    new_arr.push(arr[i] ** 2)
+  end
+  new_arr
+end
+
+def reduce_to_total(arr, start = 0)
+  sum = 0
+  for i in start...arr.length do
+    sum += arr[i]
+  end
+  sum
+end
